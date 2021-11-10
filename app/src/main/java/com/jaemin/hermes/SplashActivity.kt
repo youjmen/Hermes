6e1fcf45f997075fd885a7924db44aae73be4e29
youjmen <youjmen15@gmail.com>
2021-11-11 00:12:30 +0900

    feat : splash activity layout 없이 theme만으로 적용

--- app/src/main/java/com/jaemin/hermes/SplashActivity.kt
+++ app/src/main/java/com/jaemin/hermes/SplashActivity.kt
@@ -1,11 +1,17 @@
 package com.jaemin.hermes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
