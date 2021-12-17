21d2cb0879b691de5d148ccfc240f5c4258c23c1
youjmen <youjmen15@gmail.com>
2021-12-18 03:20:53 +0900

    codestyle : optimize import splash화면

--- app/src/main/java/com/jaemin/hermes/SplashActivity.kt
+++ app/src/main/java/com/jaemin/hermes/SplashActivity.kt
@@ -1,16 +1,13 @@
 package com.jaemin.hermes
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.jaemin.hermes.main.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
