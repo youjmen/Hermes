4e88b54a4b5f11b8590b493877e35226f680d6cc
youjmen <youjmen15@gmail.com>
2021-12-18 18:15:38 +0900

    chore: ci 실패로 원래대로 복구

--- app/src/main/java/com/jaemin/hermes/remote/BookService.kt
+++ app/src/main/java/com/jaemin/hermes/remote/BookService.kt
@@ -1,12 +1,11 @@
 package com.jaemin.hermes.remote
 
-import com.jaemin.hermes.BuildConfig
 import com.jaemin.hermes.response.BooksResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BookService {
-    @GET("ItemSearch.aspx?ttbkey=${BuildConfig.TTB_KEY}&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101")
+    @GET("ItemSearch.aspx?ttbkey=ttbyoujmen1037002&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101")
     fun searchBooks(@Query("Query") bookName : String) : Single<BooksResponse>
 }
