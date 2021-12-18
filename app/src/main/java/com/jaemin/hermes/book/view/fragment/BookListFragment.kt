2c09371ef414905d517083357f61f5b5a10204ce
youjmen <youjmen15@gmail.com>
2021-12-19 02:08:17 +0900

    feat: 검색된 책이 없는 경우, 로딩 중 경우 처리

--- app/src/main/java/com/jaemin/hermes/book/view/fragment/BookListFragment.kt
+++ app/src/main/java/com/jaemin/hermes/book/view/fragment/BookListFragment.kt
@@ -1,11 +1,12 @@
 package com.jaemin.hermes.book.view.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import com.jaemin.hermes.base.BaseViewBindingFragment
+import com.jaemin.hermes.base.EventObserver
 import com.jaemin.hermes.book.view.adapter.BookAdapter
 import com.jaemin.hermes.book.viewmodel.BookViewModel
 import com.jaemin.hermes.databinding.FragmentBookListBinding
@@ -31,11 +32,42 @@ class BookListFragment : BaseViewBindingFragment<FragmentBookListBinding>() {
 
         arguments?.getString(MainActivity.BOOK_NAME)?.let {
             viewModel.searchBooks(it)
+            binding.etSearchBooks.setText(it)
+        }
+        binding.etSearchBooks.setOnEditorActionListener { p0, actionId, p2 ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                viewModel.searchBooks(binding.etSearchBooks.text.toString())
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+        binding.ivBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+        binding.srlBooks.setOnRefreshListener {
+            viewModel.searchBooks(viewModel.bookName.value ?: binding.etSearchBooks.text.toString())
         }
         with(viewModel){
-            viewModel.books.observe(viewLifecycleOwner){
+            books.observe(viewLifecycleOwner){
                 bookAdapter.submitList(it)
+                binding.tvEmptyBooks.visibility = View.GONE
+                binding.pbLoadingBooks.visibility = View.GONE
+                binding.srlBooks.isRefreshing = false
+
             }
+            booksEmptyEvent.observe(viewLifecycleOwner, EventObserver{
+                binding.tvEmptyBooks.visibility = View.VISIBLE
+                binding.pbLoadingBooks.visibility =  View.GONE
+                binding.srlBooks.isRefreshing = false
+
+            })
+            booksErrorEvent.observe(viewLifecycleOwner, EventObserver{
+                binding.pbLoadingBooks.visibility =  View.GONE
+                binding.srlBooks.isRefreshing = false
+            })
+            booksLoadingEvent.observe(viewLifecycleOwner, EventObserver{
+                binding.pbLoadingBooks.visibility = View.VISIBLE
+            })
         }
 
     }
--- app/src/main/java/com/jaemin/hermes/book/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/jaemin/hermes/book/viewmodel/BookViewModel.kt
@@ -1,28 +1,47 @@
 package com.jaemin.hermes.book.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.jaemin.hermes.base.BaseViewModel
+import com.jaemin.hermes.base.Event
 import com.jaemin.hermes.entity.Book
 import com.jaemin.hermes.repository.BookRepository
 import com.jaemin.hermes.response.toEntity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class BookViewModel(private val bookRepository: BookRepository) : ViewModel() {
+class BookViewModel(private val bookRepository: BookRepository) : BaseViewModel() {
 
     private val _books : MutableLiveData<List<Book>> = MutableLiveData()
     val books : LiveData<List<Book>> get() = _books
 
+    private val _bookName : MutableLiveData<String> = MutableLiveData()
+    val bookName : LiveData<String> get() = _bookName
+
+    private val _booksEmptyEvent : MutableLiveData<Event<Unit>> = MutableLiveData()
+    val booksEmptyEvent : LiveData<Event<Unit>> get() = _booksEmptyEvent
+
+    private val _booksErrorEvent : MutableLiveData<Event<Unit>> = MutableLiveData()
+    val booksErrorEvent : LiveData<Event<Unit>> get() = _booksErrorEvent
+
+    private val _booksLoadingEvent : MutableLiveData<Event<Unit>> = MutableLiveData()
+    val booksLoadingEvent : LiveData<Event<Unit>> get() = _booksLoadingEvent
+
     fun searchBooks(bookName : String){
-        bookRepository.searchBooks(bookName)
+        _booksLoadingEvent.value = Event(Unit)
+        _bookName.value = bookName
+        addDisposable(bookRepository.searchBooks(bookName)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                _books.value = it.item.map { response -> response.toEntity() }
+                if (it.item.isEmpty()){
+                    _booksEmptyEvent.value = Event(Unit)
+                }
+                else {
+                    _books.value = it.item.map { response -> response.toEntity() }
+                }
             },{
-                it.printStackTrace()
-            })
+                _booksErrorEvent.value = Event(Unit)
+            }))
     }
 }
