24526765f81aead425c272b9acd7fd9b87c5932f
youjmen <youjmen15@gmail.com>
2021-12-19 02:08:45 +0900

    feat: 책 20개 불러오게 수정

--- app/src/main/java/com/jaemin/hermes/remote/BookService.kt
+++ app/src/main/java/com/jaemin/hermes/remote/BookService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BookService {
-    @GET("ItemSearch.aspx?ttbkey=ttbyoujmen1037002&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101")
+    @GET("ItemSearch.aspx?ttbkey=ttbyoujmen1037002&QueryType=Title&MaxResults=20&start=1&SearchTarget=Book&output=js&Version=20131101")
     fun searchBooks(@Query("Query") bookName : String) : Single<BooksResponse>
 }
