ad03df85d7170c6910133cc98758ba2c75d2b9b0
youjmen <youjmen15@gmail.com>
2021-12-18 17:17:38 +0900

    res: 책 viewmodel 책 검색 기능 구현

--- app/src/main/java/com/jaemin/hermes/book/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/jaemin/hermes/book/viewmodel/BookViewModel.kt
@@ -1,18 +1,28 @@
 package com.jaemin.hermes.book.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jaemin.hermes.entity.Book
 import com.jaemin.hermes.repository.BookRepository
+import com.jaemin.hermes.response.toEntity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class BookViewModel(private val bookRepository: BookRepository) : ViewModel() {
+
+    private val _books : MutableLiveData<List<Book>> = MutableLiveData()
+    val books : LiveData<List<Book>> get() = _books
 
     fun searchBooks(bookName : String){
         bookRepository.searchBooks(bookName)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-
-            },{})
+                _books.value = it.item.map { response -> response.toEntity() }
+            },{
+                it.printStackTrace()
+            })
     }
 }
