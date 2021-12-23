572e449ddfa45f4888f515fee2fdf4a38299d6b2
jaemin <youjmen15@gmail.com>
2021-12-23 18:40:17 +0900

    feat: di 주입 factory-> viewmodel로 변경, 책 정보 client 구분할 수 있게 이름 추가

--- app/src/main/java/com/jaemin/hermes/di/BookModule.kt
+++ app/src/main/java/com/jaemin/hermes/di/BookModule.kt
@@ -4,6 +4,8 @@ import com.jaemin.hermes.book.viewmodel.BookViewModel
 import com.jaemin.hermes.remote.BookService
 import com.jaemin.hermes.repository.BookRepository
 import com.jaemin.hermes.repository.BookRepositoryImpl
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
@@ -11,8 +13,8 @@ val bookModule = module {
     fun provideBookService(retrofit: Retrofit): BookService =
         retrofit.create(BookService::class.java)
     factory<BookRepository> { BookRepositoryImpl(provideBookService(get())) }
-    factory { BookViewModel(get()) }
-    single { provideBookService(get()) }
+    viewModel { BookViewModel(get()) }
+    single { provideBookService(get(named("book"))) }
 
 
 
