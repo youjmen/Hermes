9982a93203942c69371166b4bc0e208a63436f47
jaemin <youjmen15@gmail.com>
2021-12-23 18:37:20 +0900

    feat: 카카오 api 헤더 삽입한 client, 일반 client 구분, 위치, 책 정보 retrofit 구분

--- app/src/main/java/com/jaemin/hermes/di/NetworkModule.kt
+++ app/src/main/java/com/jaemin/hermes/di/NetworkModule.kt
@@ -1,27 +1,46 @@
 package com.jaemin.hermes.di
 
 import com.jaemin.hermes.BuildConfig
+import com.jaemin.hermes.datasource.KakaoInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
-        single { provideRetrofit(get()) }
-        single { provideOkHttpClient(get()) }
-        single { provideLoggingInterceptor() }
+    single(named("book")) { provideBookRetrofit(get(named("general"))) }
+    single(named("location")) { provideLocationRetrofit(get(named("location"))) }
+    single { KakaoInterceptor() }
+    single(named("general")) { provideOkHttpClient(get()) }
+    single(named("location")) { provideLocationOkHttpClient(get(),get()) }
+    single { provideLoggingInterceptor() }
 
 }
-fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+fun provideBookRetrofit(okHttpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder().baseUrl("http://www.aladin.co.kr/ttb/api/")
         .client(okHttpClient)
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create()).build()
 }
+fun provideLocationRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder().baseUrl("https://dapi.kakao.com/")
+        .client(okHttpClient)
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
 fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
-    return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    return OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+}
+fun provideLocationOkHttpClient(interceptor : KakaoInterceptor,loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .addInterceptor(loggingInterceptor)
+        .build()
 }
 fun provideLoggingInterceptor(): HttpLoggingInterceptor {
     return HttpLoggingInterceptor().apply {
