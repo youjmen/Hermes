6a28f65a24a1f85d5cca611fff090710df8d4289
jaemin <youjmen15@gmail.com>
2021-12-27 21:15:46 +0900

    feat: 메인 화면 di 모듈 추가

---
+++ app/src/main/java/com/jaemin/hermes/di/MainModule.kt
@@ -0,0 +1,9 @@
+package com.jaemin.hermes.di
+
+import com.jaemin.hermes.main.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val mainModule = module {
+    viewModel { MainViewModel(get()) }
+}
