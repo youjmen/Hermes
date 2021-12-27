ca9d06489798dd3f52ad8d578123b5a6b742062a
jaemin <youjmen15@gmail.com>
2021-12-27 21:13:35 +0900

    feat: 메인 화면에서 등록한 위치 표시 기능 추가

--- app/src/main/java/com/jaemin/hermes/main/view/activity/MainActivity.kt
+++ app/src/main/java/com/jaemin/hermes/main/view/activity/MainActivity.kt
@@ -2,19 +2,25 @@ package com.jaemin.hermes.main.view.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
 import com.jaemin.hermes.book.view.activity.BookActivity
 import com.jaemin.hermes.databinding.ActivityMainBinding
 import com.jaemin.hermes.main.view.fragment.LocationRegisterBottomSheetFragment
+import com.jaemin.hermes.main.viewmodel.LocationRegisterViewModel
+import com.jaemin.hermes.main.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel : MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel.getCurrentLocation()
         binding.etSearchBooks.setOnEditorActionListener { p0, actionId, p2 ->
             if(actionId == EditorInfo.IME_ACTION_SEARCH){
                 startActivity(Intent(this, BookActivity::class.java)
@@ -26,6 +32,13 @@ class MainActivity : AppCompatActivity() {
         binding.clLocation.setOnClickListener {
             LocationRegisterBottomSheetFragment().show(supportFragmentManager,LocationRegisterBottomSheetFragment.CLASS_NAME)
         }
+        with(viewModel){
+            currentLocation.observe(this@MainActivity){
+                Log.d("ppplace",it.toString())
+                binding.tvLocation.text = it.roadAddress
+            }
+        }
+
     }
     companion object{
         const val BOOK_NAME = "book_name"
--- app/src/main/java/com/jaemin/hermes/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/jaemin/hermes/main/viewmodel/MainViewModel.kt
@@ -1,4 +1,27 @@
 package com.jaemin.hermes.main.viewmodel
 
-class MainViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.jaemin.hermes.base.BaseViewModel
+import com.jaemin.hermes.entity.Place
+import com.jaemin.hermes.repository.LocationRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainViewModel(private val locationRepository: LocationRepository) : BaseViewModel() {
+
+    private val _currentLocation: MutableLiveData<Place> = MutableLiveData()
+    val currentLocation: MutableLiveData<Place> get() = _currentLocation
+    fun getCurrentLocation() {
+        addDisposable(locationRepository.getCurrentLocation()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                _currentLocation.value = it
+            }, {
+                it.printStackTrace()
+            })
+        )
+
+    }
 }
