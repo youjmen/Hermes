35fea91857722cc282670ca18a7cc470be831e46
jaemin <youjmen15@gmail.com>
2021-12-29 19:18:31 +0900

    feat: base activity 클래스 적용

--- app/src/main/java/com/jaemin/hermes/main/view/activity/MainActivity.kt
+++ app/src/main/java/com/jaemin/hermes/main/view/activity/MainActivity.kt
@@ -3,24 +3,18 @@ package com.jaemin.hermes.main.view.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.inputmethod.EditorInfo
-import androidx.appcompat.app.AppCompatActivity
+import com.jaemin.hermes.base.BaseViewBindingActivity
 import com.jaemin.hermes.book.view.activity.BookActivity
 import com.jaemin.hermes.databinding.ActivityMainBinding
-import com.jaemin.hermes.main.view.fragment.LocationRegisterBottomSheetFragment
-import com.jaemin.hermes.main.viewmodel.LocationRegisterViewModel
 import com.jaemin.hermes.main.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
     private val viewModel : MainViewModel by viewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        viewModel.getCurrentLocation()
         binding.etSearchBooks.setOnEditorActionListener { p0, actionId, p2 ->
             if(actionId == EditorInfo.IME_ACTION_SEARCH){
                 startActivity(Intent(this, BookActivity::class.java)
@@ -30,17 +24,25 @@ class MainActivity : AppCompatActivity() {
             return@setOnEditorActionListener false
         }
         binding.clLocation.setOnClickListener {
-            LocationRegisterBottomSheetFragment().show(supportFragmentManager,LocationRegisterBottomSheetFragment.CLASS_NAME)
+            startActivity(Intent(this, LocationRegisterActivity::class.java))
         }
         with(viewModel){
             currentLocation.observe(this@MainActivity){
-                Log.d("ppplace",it.toString())
                 binding.tvLocation.text = it.roadAddress
             }
         }
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getCurrentLocation()
+    }
     companion object{
         const val BOOK_NAME = "book_name"
+    }
+
+    override fun setViewBinding(inflater: LayoutInflater): ActivityMainBinding {
+       return ActivityMainBinding.inflate(inflater)
     }
 }
