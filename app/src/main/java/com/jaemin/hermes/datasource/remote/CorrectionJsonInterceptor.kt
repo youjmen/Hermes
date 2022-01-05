4de4302bfbf5d52cd2d2afc9d77454846e70c3a8
jaemin <youjmen15@gmail.com>
2022-01-05 14:46:54 +0900

    feat: 알라딘 api에서 제공하는 json 맨 끝에 세미콜론을 제거하기 위한 interceptor 추가

---
+++ app/src/main/java/com/jaemin/hermes/datasource/remote/CorrectionJsonInterceptor.kt
@@ -0,0 +1,31 @@
+package com.jaemin.hermes.datasource.remote
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import okhttp3.Interceptor
+import okhttp3.MediaType
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.json.JSONObject
+
+class CorrectionJsonInterceptor : Interceptor{
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+        if (response.code == 200 && response.body != null){
+
+            if (response.peekBody(MAX_BYTE).string().last()==';'){
+                val jsonBody =response.peekBody(MAX_BYTE).string().dropLast(1)
+                val contentType : MediaType = response.body!!.contentType()!!
+                val convertedBody = jsonBody.toResponseBody(contentType)
+                return response.newBuilder().body(convertedBody).build()
+            }
+        }
+        return chain.proceed(request)
+    }
+    companion object{
+        private const val MAX_BYTE = 100000000L
+    }
+}
