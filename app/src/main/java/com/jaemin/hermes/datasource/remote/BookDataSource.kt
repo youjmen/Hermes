914da1b406548e8f4e16e288ead614467b5e2685
jaemin <youjmen15@gmail.com>
2022-01-05 14:45:25 +0900

    feat: 책 정보 조회 api 연동

--- app/src/main/java/com/jaemin/hermes/datasource/remote/BookDataSource.kt
+++ app/src/main/java/com/jaemin/hermes/datasource/remote/BookDataSource.kt
@@ -6,4 +6,6 @@ import io.reactivex.rxjava3.core.Single
 interface BookDataSource {
     fun searchBooks(bookName : String) : Single<BooksResponse>
 
+    fun getBookInformation(isbn : String) : Single<BooksResponse>
+
 }
--- app/src/main/java/com/jaemin/hermes/datasource/remote/BookDataSourceImpl.kt
+++ app/src/main/java/com/jaemin/hermes/datasource/remote/BookDataSourceImpl.kt
@@ -8,4 +8,7 @@ class BookDataSourceImpl(private val bookService: BookService) : BookDataSource 
     override fun searchBooks(bookName: String): Single<BooksResponse> =
         bookService.searchBooks(bookName)
 
+    override fun getBookInformation(isbn: String): Single<BooksResponse> =
+        bookService.getBookInformation(isbn)
+
 }
