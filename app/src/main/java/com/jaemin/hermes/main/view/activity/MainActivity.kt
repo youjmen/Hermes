9f31af6f5c15980695133ded5f62a8b5f1828265
jaemin <youjmen15@gmail.com>
2022-01-07 02:46:54 +0900

    feat: 메인 화면 bottom navigation 추가, fragment 추가

--- app/src/main/java/com/jaemin/hermes/main/view/activity/MainActivity.kt
+++ app/src/main/java/com/jaemin/hermes/main/view/activity/MainActivity.kt
@@ -1,47 +1,37 @@
 package com.jaemin.hermes.main.view.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.inputmethod.EditorInfo
+import com.jaemin.hermes.R
 import com.jaemin.hermes.base.BaseViewBindingActivity
-import com.jaemin.hermes.book.view.activity.BookActivity
+import com.jaemin.hermes.book.view.fragment.BookListFragment
 import com.jaemin.hermes.databinding.ActivityMainBinding
-import com.jaemin.hermes.main.viewmodel.MainViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.jaemin.hermes.main.view.fragment.MainFragment
 
 class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
-    private val viewModel : MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.etSearchBooks.setOnEditorActionListener { p0, actionId, p2 ->
-            if(actionId == EditorInfo.IME_ACTION_SEARCH){
-                startActivity(Intent(this, BookActivity::class.java)
-                    .putExtra(BOOK_NAME,binding.etSearchBooks.text.toString()))
-                return@setOnEditorActionListener true
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, MainFragment())
+            .commit()
+        binding.bnvMain.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item_home->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fcv_main, MainFragment())
+                        .commit()
+                }
+                R.id.item_find_bookstore->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fcv_main, BookListFragment())
+                        .commit()
+                }
+
+
             }
-            return@setOnEditorActionListener false
+            return@setOnItemSelectedListener true
         }
-        binding.clLocation.setOnClickListener {
-            startActivity(Intent(this, LocationRegisterActivity::class.java))
-        }
-        with(viewModel){
-            currentLocation.observe(this@MainActivity){
-                binding.tvLocation.text = it.roadAddress
-            }
-        }
-
     }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.getCurrentLocation()
-    }
-    companion object{
-        const val BOOK_NAME = "book_name"
-    }
-
     override fun setViewBinding(inflater: LayoutInflater): ActivityMainBinding {
        return ActivityMainBinding.inflate(inflater)
     }
