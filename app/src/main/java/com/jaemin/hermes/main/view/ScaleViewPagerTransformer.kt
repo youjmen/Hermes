849bde0c8a8258d8251a821565eab3673d4034d2
jaemin <youjmen15@gmail.com>
2022-01-11 16:12:34 +0900

    modify: position 절댓값에 따라 alpha값 서서히 변화시키게 변경

--- app/src/main/java/com/jaemin/hermes/main/view/ScaleViewPagerTransformer.kt
+++ app/src/main/java/com/jaemin/hermes/main/view/ScaleViewPagerTransformer.kt
@@ -1,49 +1,24 @@
 package com.jaemin.hermes.main.view
 
-import android.util.Log
 import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
+import kotlin.math.max
 
-private const val MIN_SCALE = 0.85f
+private const val MIN_SCALE = 0.8f
 private const val MIN_ALPHA = 0.5f
 
 class ScaleViewPagerTransformer(private val offsetPx : Int) : ViewPager2.PageTransformer {
     override fun transformPage(page: View, position: Float) {
         page.translationX = position * -offsetPx
         page.apply {
-            val pageWidth = width
-            val pageHeight = height
-            Log.d("dddddddpageWidth", width.toString())
-            Log.d("dddddddpageHeight", height.toString())
-            Log.d("dddddddpagepos", position.toString())
-            Log.d("dddddddpageview", page.toString())
-
+            alpha = 1 - abs(position) + MIN_ALPHA
             when {
-                position < -1 -> { // [-Infinity,-1)
-                    // This page is way off-screen to the left.
-                    alpha = 0f
-                }
-                position <= 1 -> { // [-1,1]
-                    // Modify the default slide transition to shrink the page as well
-                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
-//                    val vertMargin = pageHeight * (1 - scaleFactor) / 2
-//                    val horzMargin = pageWidth * (1 - scaleFactor) / 2
-//                    translationX = if (position < 0) {
-//                        horzMargin - vertMargin / 2
-//                    } else {
-//                        horzMargin + vertMargin / 2
-//                    }
-                    // Scale the page down (between MIN_SCALE and 1)
+                position <= 1 -> {
+                    val scaleFactor = max(MIN_SCALE, 1 - abs(position))
                     scaleX = scaleFactor
                     scaleY = scaleFactor
 
-                    // Fade the page relative to its size.
-                    alpha = (MIN_ALPHA +
-                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
-                }
-                else -> { // (1,+Infinity]
-                    // This page is way off-screen to the right.
-                    alpha = 0f
                 }
             }
         }
