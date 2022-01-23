fcbb7c9a70320eee8522761e0c071eb4a17b65fe
jaemin <youjmen15@gmail.com>
2022-01-24 01:13:44 +0900

    feat: 서점 조회 화면 추가

--- app/src/main/java/com/jaemin/hermes/di/BookstoreModule.kt
+++ app/src/main/java/com/jaemin/hermes/di/BookstoreModule.kt
@@ -1,11 +1,14 @@
 package com.jaemin.hermes.di
 
 import com.jaemin.hermes.book.viewmodel.CheckStockViewModel
+import com.jaemin.hermes.bookstore.viewmodel.BookstoreSearchViewModel
 import com.jaemin.hermes.datasource.remote.KyoboBooksScraper
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val bookstoreModule = module {
-    factory { CheckStockViewModel(get(), get()) }
+    viewModel { CheckStockViewModel(get(), get()) }
+    viewModel { BookstoreSearchViewModel(get()) }
     factory { KyoboBooksScraper() }
 
 }
