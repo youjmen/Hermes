d32cc297647c1a3ee13d2d135cc27725cd8f447d
jaemin <youjmen15@gmail.com>
2022-02-17 02:55:11 +0900

    feat: 베스트셀러 pagingSource 추가

---
+++ app/src/main/java/com/jaemin/hermes/datasource/remote/BestSellerPagingSource.kt
@@ -0,0 +1,35 @@
+package com.jaemin.hermes.datasource.remote
+
+import android.util.Log
+import androidx.paging.PagingState
+import androidx.paging.rxjava3.RxPagingSource
+import com.jaemin.hermes.BuildConfig
+import com.jaemin.hermes.response.BookResponse
+import io.reactivex.rxjava3.core.Single
+import java.lang.Exception
+
+class BestSellerPagingSource(private val bookService: BookService) : RxPagingSource<Int, BookResponse>() {
+    override fun loadSingle(params: LoadParams<Int>): Single<LoadResult<Int, BookResponse>>{
+        val page = params.key ?: 1
+        Log.d("okhttppages", "calls twice $page")
+        if (page < 1){
+            return Single.just(LoadResult.Error(Exception("page under 0")))
+        }
+        return bookService.getBestSellers(BuildConfig.TTB_KEY, page)
+            .map {
+                Log.d("okhttppages here?", "calls twice $page")
+                LoadResult.Page(it.item, page-1, page + 1) as LoadResult<Int, BookResponse>
+            }.onErrorReturn {
+                LoadResult.Error(it)
+            }
+
+    }
+    override fun getRefreshKey(state: PagingState<Int, BookResponse>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+
+    }
+
+}
