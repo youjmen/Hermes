1f9516ceaee9ae09eb471aacb55cf59de82048e4
jaemin <youjmen15@gmail.com>
2022-02-17 02:54:52 +0900

    feat: 신간 주목할만한 도서 pagingSource 추가

---
+++ app/src/main/java/com/jaemin/hermes/datasource/remote/NewSpecialBooksPagingSource.kt
@@ -0,0 +1,33 @@
+package com.jaemin.hermes.datasource.remote
+
+import android.util.Log
+import androidx.paging.PagingState
+import androidx.paging.rxjava3.RxPagingSource
+import com.jaemin.hermes.BuildConfig
+import com.jaemin.hermes.response.BookResponse
+import io.reactivex.rxjava3.core.Single
+import java.lang.Exception
+
+class NewSpecialBooksPagingSource(private val bookService: BookService) : RxPagingSource<Int, BookResponse>() {
+    override fun loadSingle(params: LoadParams<Int>): Single<LoadResult<Int, BookResponse>>{
+        val page = params.key ?: 1
+        if (page < 1){
+            return Single.just(LoadResult.Error(Exception("page under 0")))
+        }
+        return bookService.getNewSpecialBooks(BuildConfig.TTB_KEY, page)
+            .map {
+                LoadResult.Page(it.item, page-1, page + 1) as LoadResult<Int, BookResponse>
+            }.onErrorReturn {
+                LoadResult.Error(it)
+            }
+
+    }
+    override fun getRefreshKey(state: PagingState<Int, BookResponse>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+
+    }
+
+}
